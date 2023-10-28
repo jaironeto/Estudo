66c93ae7873ae734b1ca00503071e35d9e8e44e1
Jairo Neto <jaironeto788@gmail.com>
2023-10-28 10:41:34 -0300

    changed 24 files

--- teste.js
+++ teste.js
@@ -1,6 +1,4 @@
-```
-- https://react.dev/learn/describing-the-ui
-```
+_react
 
-_
-_
+// uma função, no caso uma declaração de função (sem ser de metodo, ora uma closure.. e um objeto 
+// unico e independente da instaciação
