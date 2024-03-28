2a26ca6abe550989572a82a2d1c95ba18e9f34de
Jairo <jaironeto788@gmail.com>
2024-03-27 22:14:18 -0300

    changed 3 files

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿public abstract class Person{
+  private string _name;
+
+  public Person(string name){
+    this._name = name;
+  }
+
+  public string name{
+    get => this._name;
+    set => this._name = name;
+  }
+
+  public abstract string sobrenome {get;}
+}
+
+public class Sobre : Person{
+  private string _sobrenome;
+
+  public Sobre(string nome, string sobrenome): base(nome){
+    this._sobrenome = sobrenome;
+  }
+
+  public override string sobrenome {
+    get => this._sobrenome;
+  }
+}
