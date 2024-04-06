75d5913df85c9e3e303d4cbc58536d60abcf563a
Jairo <jaironeto788@gmail.com>
2024-04-06 01:05:12 -0300

    modified Program.cs and Status.md

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,1 +1,10 @@
 ﻿
+MyClass myclass = new MyClass(66); 
+
+myclass.method();
+public class MyClass(int valor){
+
+  public void method(){
+    System.Console.WriteLine(valor);
+  }
+}
--- Status.md
+++ Status.md
@@ -1,6 +1,9 @@
 🧪⚠️
 # C# code example pratico
-1.7
-1.8
+> 1.7
+> 1.8
 1.9
 1.2
+
+# React codigo
+1.1
