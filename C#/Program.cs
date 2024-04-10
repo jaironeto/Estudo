a245d86998ec8fe5d7b1f7ae16be0f7c2e8285ab
Jairo <jaironeto788@gmail.com>
2024-04-09 21:09:13 -0300

    changed 4 files

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,19 +1,1 @@
 ﻿
-carro.OtherPerson carrin = new carro.OtherPerson();
-
-System.Console.WriteLine(Person.method());
-System.Console.WriteLine(carrin.method());
-
-public class Person{
-  public static string method(){
-    return "ford";
-  }
-}
-
-namespace carro {
-  public class OtherPerson{
-    public string method(){
-      return "vrum";
-    }
-  }
-}  
--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Person from "./teste"
+import Gallery from "./teste"
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <Person/>
+    <Gallery/>
   </React.StrictMode>
 );
 
