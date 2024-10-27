e671a339849b470ad8be8dfdf9a5fc76fca4c956
Jairo <jaironeto788@gmail.com>
2024-10-27 18:15:01 -0300

    modified z.js in .

--- z.js
+++ z.js
@@ -1,14 +1,22 @@
 class myClass{
+  #campoPrivado = 0;
+
   constructor(idade, profissao){
     this.idade = idade;
     this.profissao = profissao;
   }
 
-  func(){
-    console.log(this.idade + " " + this.profissao);
+  get func(){
+    return this.#campoPrivado;
   }
+
+  set func(value){
+    this.#campoPrivado = value;
+  }
+
+
 }
 
 var obj = new myClass(24, "programador");
-
-obj.func();
+obj.func = 30
+console.log(obj.func);
