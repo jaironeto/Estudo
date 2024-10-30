0ae59b7e3747240fd4ea5695cdf36f4a2dfe5ae8
Jairo <jaironeto788@gmail.com>
2024-10-30 20:25:22 -0300

    modified z.java in .

--- z.java
+++ z.java
@@ -8,10 +8,16 @@ class z extends automovel {
     public static void main(String[] args) {
         int[] myarray = {100, 3000, 200, 10, 20, 30, 10};
 
+        labelName:
         for (int value : myarray) {
             System.out.println("valor: " + value);
+            for (int value2 : myarray) {
+                System.out.println("valor2: " + value2);
+
+                if (value2 == 200) {
+                    break labelName;
+                }
+            }
         }
     }
 }
-
-_
