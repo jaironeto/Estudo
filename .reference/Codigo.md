e2062cb7d4c9b56fe3fdca3d1b5ab453a89b9182
Jairo <jaironeto788@gmail.com>
2024-11-21 20:26:41 -0300

    modified Codigo.md and z.java

--- .reference/Codigo.md
+++ .reference/Codigo.md
@@ -16,7 +16,7 @@
 15. for loop aprimorado
 16. label e uma break encerrando a label externa
 17. continue palavra-chave
-18. construtor e encapsulamento
+>18. construtor e encapsulamento
 19. declaração simples de um metodo
 20. sobrecarga de metodo
 21. metodo com um array como parametro
--- z.java
+++ z.java
@@ -5,9 +5,13 @@ class myClass {
         method(11, 23, 34);
         int[] arraymy = new int[10];
         arraymy[0] = 3;
+        int[] arr1 = {10, 30, 22, 3, 6, 66};
         
-        System.out.println(arraymy[0]);
+        int[][] arr2 = {{1,3}, {5,4}};
+        System.out.println(arr2[0][0]);
 
+        Person obj = new Person("jairo", "marcos");
+        obj.method();
     }
 
     public static void method(int... args){
@@ -17,5 +21,15 @@ class myClass {
 };
 
 class Person{
-    
+    private String name1;
+    private String name2;
+
+    public Person(String name1, String name2){
+        this.name1 = name1;
+        this.name2 = name2;
+    }
+
+    public void method(){
+        System.out.println(this.name1 + " " + this.name2);
+    }
 }
