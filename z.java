2afcd09715160410c9b3978b7325c0fb1d28d138
Jairo <jaironeto788@gmail.com>
2024-11-22 19:14:29 -0300

    modified z.java in .

--- z.java
+++ z.java
@@ -3,14 +3,10 @@ class myClass {
 
     public static void main(String[] args) {
 
-        Person extern = new Person();
-        Person.Intern intern = extern.new Intern();
-        intern.metodo();
+        int[] arr1 = new int[10];
+        arr1[0] = 66;
 
-        myInterface objInterface = new Person();
-        objInterface.executar();
-        objInterface.method();
-        
+        int[] arr2 = {123, 34, 66};
     }
 
     public static void method(int... args){
@@ -19,46 +15,9 @@ class myClass {
 
 };
 
-interface myInterface{
-    public void executar();
+class Pai{
+
 }
-class Person implements myInterface{
-    private String name1;
-    private String name2;
-    public String name3;
-    public static String name4;
-    public String name5;
+class Filho extends Pai{
 
-    public void executar(){
-        System.out.println("interface checked");
-    }
-
-    public Person(){
-        this("jairo", "joice", "annaysa");
-        
-    }
-    public Person(String name1, String name2, String name3){
-        this.name1 = name1;
-        this.name2 = name2;
-        this.name3 = name3;
-
-    }
-    
-    class Intern{
-        public void metodo(){
-            System.out.println(name1 + " intern");
-        }
-    }
-
-    static {
-        Person.name4 = "Gustavo";
-    }
-
-    {
-        this.name5 = "vrummmmmmmm";
-    }
-
-    public void method(){
-        System.out.println(this.name1 + " " + this.name2 + " " + this.name3);
-    }
 }
