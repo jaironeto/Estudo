56731e364a3767250cba2fc521311c38173d05b7
Jairo <jaironeto788@gmail.com>
2025-01-05 23:44:30 -0300

    modified z.java in .

--- z.java
+++ z.java
@@ -1,10 +1,21 @@
 class Program{
-  
+    public static void main(String[] args) {
+        
+        final String name1 = "jairo";
+
+        int[] array1 = new int[10];
+        array1[0] = 2;
+
+        int[] array2 = {20,30,44,11};
+        int[][] array3 = new int[20][10];
+        int[][] array4 = {{23,2}, {33,2}};
+
+        String name2 = "jairo" + " " + "marcos";
+    }
 }
 class Pai{
 
 }
-
 class Filho extends Pai{
 
 }
