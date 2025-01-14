9c3ba6430ba8f81de100f123065608e9b3578f5f
Jairo <jaironeto788@gmail.com>
2025-01-14 17:02:51 -0300

    modified Codigo.md and Program.cs

--- MeuProjeto/Program.cs
+++ MeuProjeto/Program.cs
@@ -1,4 +1,5 @@
-﻿enum DiasDaSemana
+﻿
+enum DiasDaSemana
 {
   Domingo,
   Segunda,
@@ -13,15 +14,40 @@ class Program
 {
   static async Task Main()
   {
-
-    Class1 obj = new Class1("jairo") { nome = "João" };
-    System.Console.WriteLine(obj.nome);
-    System.Console.WriteLine(obj.nome2);
-    DiasDaSemana dia = DiasDaSemana.Segunda;
-    System.Console.WriteLine(dia);
-    await Task.Delay(3000);
-    System.Console.WriteLine("Hello World!");
+    Class4 obj = new Class4();
+    System.Console.WriteLine(Class4.nome);
   }
+}
+
+static class Class6{
+  public const string name = "vitor";
+  public static readonly string name2 = "marta";
+
+  static Class6(){
+    Class6.name2 = "jairo";
+  }
+
+}
+
+class Class4{
+  public const string nome = "aviao";
+  public virtual void method(){
+    Console.WriteLine("jairo");
+  }
+}
+abstract class Class3 : Class4{
+  public abstract void methodAbstract();
+  public override abstract void method();
+
+}
+class Class2{
+  public (string, string) method(){
+    return ("jairo3", "vrum");
+
+  }
+}
+record Record1(string nome){
+  public string nome2 { get; set; }
 }
 
 class Class1(string nome)
