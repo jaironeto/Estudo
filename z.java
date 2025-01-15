2ce7de6cd7a6dc16f3caf55139c106b659906741
Jairo <jaironeto788@gmail.com>
2025-01-15 14:02:55 -0300

    modified 3 files in .

--- z.java
+++ z.java
@@ -1,75 +1,32 @@
-class Pai implements interface1{
-
-  class Inter{
-    public void method(){
-      System.out.println("Inter");
-    }
-  }
-
-  private String name;
-
-  public void method(){
-    System.out.println("interface1");
-  }
-
-  public Pai(){
-    this("jairo");
-  }
-
-  public Pai(String name){
-    this.name = name;
-  }
-  public void method(int... a){
-    System.out.println(a[0]);
-  }
-}
-interface interface1{
-  void method();
-}
-
-enum enum1{
-  A, B, C;
-}
-
-interface interface2{
-  default void method(){
-    System.out.println("interface2");
-  }
-}
-
-interface interface3{
-  default void method(){
-    System.out.println("interface3");
-  }
-}
-
-class class3 implements interface2, interface3{
- @Override
-  public void method(){
-    interface2.super.method();
-    interface3.super.method();
-  }
-}
 public class z{
   public static void main(String[] args){
-    Pai p = new Pai();
-    Pai.Inter i = p.new Inter();
+    int[] array1 = new int[10];
+    int[] array2 = {20,33,22,33};
+    int[][] array3 = new int[3][2];
+    int[][] array4 = {{33,22,1}, {35,4}};
 
-    interface1 i1 = new Pai();
-    interface1 i2 = new interface1(){
-      public void method(){
-        System.out.println("interface2");
-      }
-    };
+    Class1 obj = new Class1("vrum");
+    obj.method(array2);
+  }
+}
 
-    enum1 e = enum1.A;
+class Class1{
+  private String name;
+  public Class1(String name){
+    this.name = name;
+  }
 
-    interface1 soma = () -> System.out.println("soma");
-    interface1 mult = () ->{ 
-      System.out.println("mult");
-      System.out.println("mult");
-    };
-    soma.method();
-    mult.method();
+  public void method(){
+    System.out.println("method");
+  }
+
+  public void method(int i){
+    System.out.println("method" + i);
+  }
+  
+  public void method(int[] i){
+    for(int a : i){
+      System.out.println("method for" + a );
+    }
   }
 }
