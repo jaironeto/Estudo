102d8e80d68c554fcca86d8d7413a02061d502a3
Jairo <jaironeto788@gmail.com>
2025-01-15 14:20:26 -0300

    modified z.java in .

--- z.java
+++ z.java
@@ -1,32 +1,74 @@
-public class z{
-  public static void main(String[] args){
-    int[] array1 = new int[10];
-    int[] array2 = {20,33,22,33};
-    int[][] array3 = new int[3][2];
-    int[][] array4 = {{33,22,1}, {35,4}};
 
-    Class1 obj = new Class1("vrum");
-    obj.method(array2);
-  }
+public class z {
+
+    public static void main(String[] args) {
+
+        inter1 i = new inter1() {
+            @Override
+            public void method() {
+                System.out.println("metodo anonimo");
+            }
+        };
+
+        inter1 soma = () -> {
+            System.out.println("teste1");
+            System.out.println("teste3");
+        };
+        inter1 mult = () -> System.out.println("teste2");
+
+        soma.method();
+
+    }
 }
 
-class Class1{
-  private String name;
-  public Class1(String name){
-    this.name = name;
-  }
+interface inter1 {
 
-  public void method(){
-    System.out.println("method");
-  }
+    void method();
+}
 
-  public void method(int i){
-    System.out.println("method" + i);
-  }
-  
-  public void method(int[] i){
-    for(int a : i){
-      System.out.println("method for" + a );
+class Class2 implements inter1 {
+
+    public int campo;
+
+    public void method() {
+        System.out.println("method");
     }
-  }
+
+    public void method2() {
+        System.out.println("method2");
+    }
+}
+
+class Extern {
+
+    class Inter {
+
+    }
+}
+
+class Class1 {
+
+    private String name;
+
+    public Class1() {
+        this("padrao");
+    }
+
+    public Class1(String name) {
+        this.name = name;
+    }
+
+    public void method() {
+        System.out.println("method");
+    }
+
+    public void method(int i, int... numero) {
+        System.out.println("method" + i);
+    }
+
+    public void method(int[] i) {
+        for (int a : i) {
+            System.out.println("method for" + a);
+        }
+    }
 }
