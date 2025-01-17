f249d33036cdab46863f586c9b19c58d70c20798
Jairo <jaironeto788@gmail.com>
2025-01-17 18:43:35 -0300

    modified z.java in .

--- z.java
+++ z.java
@@ -1,78 +1,25 @@
-
-public class z {
-
+public class z{
     public static void main(String[] args) {
-
-        Class3 i = new Class3();
-        i.method();
-        var o = 3;
-    }
-}
-class Class3 implements inter2, inter3{
-  public void method(){
-    inter2.super.method();
-    inter3.super.method();
-  }
-}
-interface inter2{
-  default void method(){
-    System.out.println("inter2");
-  }
-}
-interface inter3{
-  default void method(){
-    System.out.println("inter3");
-  }
-}
-
-interface inter1 {
-
-    void method();
-}
-
-class Class2 implements inter1 {
-
-    public int campo;
-
-    public void method() {
-        System.out.println("method");
-    }
-
-    public void method2() {
-        System.out.println("method2");
+        Extern ex = new Extern();
+        Extern.Intern in = ex.new Intern();
+        in.method();
     }
 }
 
-class Extern {
-
-    class Inter {
-
-    }
-}
-
-class Class1 {
-
-    private String name;
-
-    public Class1() {
-        this("padrao");
-    }
-
-    public Class1(String name) {
-        this.name = name;
-    }
-
-    public void method() {
-        System.out.println("method");
-    }
-
-    public void method(int i, int... numero) {
-        System.out.println("method" + i);
-    }
-
-    public void method(int[] i) {
-        for (int a : i) {
-            System.out.println("method for" + a);
+class Extern{
+    class Intern{
+        public void method(){
+            System.out.println("class intern");
         }
+    }
+}
+class Class2{
+    public String campo1;
+}
+
+class Class1{
+    public Class2 method(){
+        Class2 obj = new Class2();
+        return obj;
     }
 }
