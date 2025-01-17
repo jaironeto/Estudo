603895b6ff08184ef3781b29dc7bed0d6fd3aa2d
Jairo <jaironeto788@gmail.com>
2025-01-17 18:53:29 -0300

    modified .codigo.md and z.java in .

--- z.java
+++ z.java
@@ -1,24 +1,48 @@
-public class z{
+
+public class z {
+
     public static void main(String[] args) {
-        Extern ex = new Extern();
-        Extern.Intern in = ex.new Intern();
+        Inter in = new Inter() {
+            @Override
+            public void method() {
+                System.out.println("jaa");
+            }
+        };
+
         in.method();
-    }
+
+        Inter an4 = () -> System.out.println("aaa");
+        an4.method();
+
+        _
+
+    } // fim main
+
 }
 
-class Extern{
-    class Intern{
-        public void method(){
+interface Inter {
+
+    void method();
+}
+
+class Extern {
+
+    class Intern {
+
+        public void method() {
             System.out.println("class intern");
         }
     }
 }
-class Class2{
+
+class Class2 {
+
     public String campo1;
 }
 
-class Class1{
-    public Class2 method(){
+class Class1 {
+
+    public Class2 method() {
         Class2 obj = new Class2();
         return obj;
     }
