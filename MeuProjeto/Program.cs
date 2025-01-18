a25ffad698e37b258a614eeb0f720af0c1d8f0a8
Jairo <jaironeto788@gmail.com>
2025-01-18 20:21:14 -0300

    modified Program.cs and z.java

--- MeuProjeto/Program.cs
+++ MeuProjeto/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+
+class Program{
+  public static void Main(){
+    Class1 obj = new Class1();
+    ref int valorRef = ref obj.methodRef();
+    System.Console.WriteLine(valorRef);
+    valorRef = 33;
+    System.Console.WriteLine(valorRef);
+
+    obj.methodRef(ref valorRef);
+        System.Console.WriteLine(valorRef);
+
+  }
+}
+class Class1{
+  private int value = 2;
+  public ref int methodRef(){
+    
+    return ref this.value;
+  }
+
+  public ref int methodRef(ref int b){
+    b += 10;
+    return ref b;
+  }
+}
