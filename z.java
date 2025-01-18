25020c804c98721ea7ef0551dbe347fe6208112a
Jairo <jaironeto788@gmail.com>
2025-01-18 19:37:12 -0300

    modified .codigo.md and z.java in .

---
+++ z.java
@@ -0,0 +1,12 @@
+public class z {
+
+    public static void main(String[] args) {
+       
+       In i = () -> System.out.println("haha");
+       
+    }
+}
+
+interface In {
+    public void method();
+}
