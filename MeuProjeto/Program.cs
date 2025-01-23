cd52ad9bc5a1f038eeadcd5ee3d41bfce4b94f92
Jairo <jaironeto788@gmail.com>
2025-01-23 19:43:14 -0300

    modified 4 files

--- MeuProjeto/Program.cs
+++ MeuProjeto/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿class Program{
+  public static void Main(){
+    Class1 obj = new Class1(){propriedade1 = "vrum"}; 
+  }
+}
+
+class Class1{
+  public required string propriedade1 { get; set; } 
+}
--- z.java
+++ z.java
@@ -1,15 +1,25 @@
 public class z{
   public static void main(String[] args){
+    int[] array1 = {1,3,4,5,6};
 
+    for(int i : array1){
+      System.out.println(i);
+    }
+
+    inter in = new inter(){
+      public void method(){
+        System.out.println("teste");
+      }
+    };
+
+    inter in1 = () -> System.out.println("vrum");
+
+    in1.method();
   }
+
+  public static void Method1(int... i){}
 }
 
-class A{
-  private String name;
-
-
-}
-
-class B extends A{
-
+interface inter{
+  public void method();
 }
