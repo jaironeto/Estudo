ccf3bb18010842ba0f9f7b932d6ab9906c808b1a
Jairo <jaironeto788@gmail.com>
2025-01-26 17:51:40 -0300

    modified 5 files

--- MeuProjeto/Program.cs
+++ MeuProjeto/Program.cs
@@ -1,9 +1,26 @@
 ﻿class Program{
   public static void Main(){
-    Class1 obj = new Class1(){propriedade1 = "vrum"}; 
+    Class1 obj = new Class1();
+
+    name del = obj.method;
+
+    System.Console.WriteLine(del("jairo"));
   }
 }
+  _
+public class Class1{
 
-class Class1{
-  public required string propriedade1 { get; set; } 
+  private string[] name = new string[10];
+
+  public string this[int index]{
+    get{
+      return this.name[index];
+    }
+    set => this.name[index] = value;
+  }
+
+  public string method(string i){
+    return i + "vrum";
+  }
 }
+public delegate string name(string i);
--- z.java
+++ z.java
@@ -1,25 +1,22 @@
 public class z{
   public static void main(String[] args){
-    int[] array1 = {1,3,4,5,6};
-
-    for(int i : array1){
-      System.out.println(i);
-    }
-
-    inter in = new inter(){
-      public void method(){
-        System.out.println("teste");
-      }
-    };
-
-    inter in1 = () -> System.out.println("vrum");
-
-    in1.method();
+    Class1 obj = new Class1();
+    obj.method2();
   }
-
-  public static void Method1(int... i){}
 }
 
-interface inter{
-  public void method();
+class Class1{
+  public String campo1 = "ford";
+
+  class Class2{
+    public void method(){
+      System.out.println(campo1);
+    }
+  }
+
+  public void method2(){
+    Class2 obj = new Class2();
+    this.campo1 = "vrum";
+    obj.method();
+  }
 }
