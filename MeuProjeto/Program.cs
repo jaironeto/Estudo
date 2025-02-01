d1d86412fa56e6b54addc432ca226a23363db058
Jairo <jaironeto788@gmail.com>
2025-02-01 00:11:23 -0300

    modified 6 files

--- MeuProjeto/Program.cs
+++ MeuProjeto/Program.cs
@@ -1,26 +1,37 @@
 ﻿class Program{
   public static void Main(){
-    Class1 obj = new Class1();
+    Class1 obj = new Class1("name"){prop1 = "ford"};
 
-    name del = obj.method;
+    var obj2 = new {name = "ford", vacilidade = "mega"};
 
-    System.Console.WriteLine(del("jairo"));
+    Class2<string> obj3 = new Class2<string>("jairo");
+    
   }
 }
-  _
-public class Class1{
 
-  private string[] name = new string[10];
+class Class2<T>{
+  public T name;
+
+  public Class2(T name){
+    this.name = name;
+  }
+}
+class Class1(string a){
+
+  private string[] obj = new string[10];
 
   public string this[int index]{
     get{
-      return this.name[index];
+      return obj[index] ;
     }
-    set => this.name[index] = value;
-  }
 
-  public string method(string i){
-    return i + "vrum";
+    set{
+      obj[index] = value;
+    }
+  }
+  public required string prop1 {get; set;} = a;
+
+  public void method1(params string[] i){
+
   }
 }
-public delegate string name(string i);
--- z.java
+++ z.java
@@ -1,6 +1,16 @@
 public class z{
   public static void main(String[] args){
-    Class1 obj = new Class1();
+
+
+    Class1 obj = new Class1(){
+      public String campo1 = "mard";
+
+      @Override
+      public void method2(){
+        System.out.println(this.campo1);
+      }
+    };
+
     obj.method2();
   }
 }
@@ -8,15 +18,7 @@ public class z{
 class Class1{
   public String campo1 = "ford";
 
-  class Class2{
-    public void method(){
-      System.out.println(campo1);
-    }
-  }
-
   public void method2(){
-    Class2 obj = new Class2();
     this.campo1 = "vrum";
-    obj.method();
   }
 }
