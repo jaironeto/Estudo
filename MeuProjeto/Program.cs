381d37cf54967743c86a0dd767f6e452d14d893e
jairo neto <jaironeto788@gmail.com>
2025-02-17 14:24:20 -0300

    modified 3 files

--- MeuProjeto/Program.cs
+++ MeuProjeto/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿class Program{
+  public static void Main(){
+    System.Console.WriteLine("vrum");
+
+    var i = new[] {new {name = "jairo"}};
+
+        System.Console.WriteLine(i[0].name);
+
+  }
+}
--- z.js
+++ z.js
@@ -1,1 +1,18 @@
 var matriz = [[12,2], [33,3]];
+
+var obj = {
+  name1: "jairo",
+  name2: "marcos"
+}
+
+for(const i in obj){
+  console.log(obj[i])
+}
+
+class MyClass{
+  construtor(...args){
+    this.name1 = args[0];
+  }
+}
+
+_java
