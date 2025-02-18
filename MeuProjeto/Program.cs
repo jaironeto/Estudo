e035db95f1dadd515dc8e6d902473bd17176f321
jairo neto <jaironeto788@gmail.com>
2025-02-18 13:39:48 -0300

    modified 5 files

--- MeuProjeto/Program.cs
+++ MeuProjeto/Program.cs
@@ -1,10 +1,32 @@
-﻿class Program{
-  public static void Main(){
+﻿class Program
+{
+  public static void Main()
+  {
     System.Console.WriteLine("vrum");
 
-    var i = new[] {new {name = "jairo"}};
+    var i = new[] { new { name = "jairo" } };
 
-        System.Console.WriteLine(i[0].name);
+    System.Console.WriteLine(i[0].name);
 
+    var obj = new { jairo = "vrum"};
+
+    var nu = 20;
+
+  }
+}
+
+public record MyRecord(string name1, string name2);
+
+public record MyRecord2()
+{
+
+}
+
+class MyExtensions{
+
+  public string this[int indice]{
+      get{
+        return "vrum"; _
+      }
   }
 }
