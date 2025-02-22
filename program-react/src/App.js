6dbc6e31be623f0c0be8856193c8d4005a934621
jairo neto <jaironeto788@gmail.com>
2025-02-21 23:52:01 -0300

    modified 5 files

--- program-react/src/App.js
+++ program-react/src/App.js
@@ -1,52 +1,18 @@
+import Profile from "./Teste"
 /*
       src='https://i.imgur.com/MK3eW3As.jpg'
       '✔
 */
-import Profile from "./Teste"
 
-var theme = {
-  backgroundColor: "red",
-  color: "blue"
+var style = {
+  color: "red"
 }
-
-var name = "jairo"
-
-function Card({children}){
-  return(
-    <div>
-      <p>teste1</p>
-      {children}
-    </div>    
-  )
-}
-
-function Teste({condicao = false}){
-  var obj = null;
-  if(condicao){
-    obj = (
-    <div>
-      <p>is true</p>
-    </div>);
-  }
-
-  return obj;
-}
-
-export default function App1(){
-  return(
-    <>
-      <section className="teste1">
-        <h1> {name} teste</h1>
-        <Profile/>
-        <Profile/>
-        <Card>
-          <p>vrummm</p>
-        </Card>
-        <Teste condicao={true}/>
-        <Teste condicao={false}/>
-        <Teste condicao={true}/>
-
-      </section> 
-    </>
-  );
+export default function App1() {
+  return (
+    <div style={style}>
+      <h1>teste</h1>
+      <p>{false ? "vrum" : "tu"}</p>
+        <Profile />
+      </div>
+        )
 }
