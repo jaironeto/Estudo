c14465d212fc1c165d934859b4746c7d605a0902
jairo neto <jaironeto788@gmail.com>
2025-03-08 20:02:38 -0300

    modified z.java deleted ..cronograma.md

---
+++ z.java
@@ -0,0 +1,12 @@
+public class z{
+  private String name1;
+
+  public z(String name1){
+    this("jairo", "vrum");
+    this.name1 = name1;
+  }
+
+  public z(String name1, String name2){
+    this.name1 = name1;
+  }
+}
