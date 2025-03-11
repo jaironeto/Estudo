5bdeb5a5462b177587fcf4c85446579b6008764c
jairo neto <jaironeto788@gmail.com>
2025-03-11 00:18:30 -0300

    modified z.java in .

--- z.java
+++ z.java
@@ -1,12 +1,2 @@
-public class z{
-  private String name1;
-
-  public z(String name1){
-    this("jairo", "vrum");
-    this.name1 = name1;
-  }
-
-  public z(String name1, String name2){
-    this.name1 = name1;
-  }
-}
+Extern ex = new Extern();
+Extern.Intern in = ex.new Intern();
