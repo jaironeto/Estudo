196bca98a36e967717cfd34a195c75ca2560e5ec
jairo neto <jaironeto788@gmail.com>
2025-03-11 01:09:29 -0300

    modified 5 files

--- z.js
+++ z.js
@@ -1,18 +1,53 @@
-var matriz = [[12,2], [33,3]];
+var obj = new Promise((resolve, reject)=>{
+  resolve("teste");
+})
 
-var obj = {
-  name1: "jairo",
-  name2: "marcos"
-}
+obj.then((value)=>{
+  console.log(value);
+})
 
-for(const i in obj){
-  console.log(obj[i])
-}
+function teste(){
+  var index = 0;
 
-class MyClass{
-  construtor(...args){
-    this.name1 = args[0];
+  return function(){
+    console.log(index++);
   }
 }
 
-_java
+var t = teste();
+t();
+t();
+t();
+
+class MyClass{
+  constructor(...name){
+    this.name = name[0];
+  }
+}
+
+var myClass = new MyClass("jairo");
+console.log(myClass.name);
+
+function MyConstrutor(name){
+  this.name = name;
+}
+
+var myConstrutor = new MyConstrutor("jairo");
+MyConstrutor.prototype.getName = function(){
+  return this.name;
+}
+
+console.log(myConstrutor.getName());
+
+var obj = { name: "jairo" };
+obj.prototype.prof = "developer";
+console.log(obj.prof);
+
+var obj ={
+  name: "jairo",
+  teste: function(){
+    console.log(this.name);
+  }
+}
+
+_jav
