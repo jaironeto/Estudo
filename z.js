e5b6017a27533fcb23376856760a2ab56d488e98
jairo neto <jaironeto788@gmail.com>
2025-04-15 16:51:20 -0300

    modified z.js in .

--- z.js
+++ z.js
@@ -5,3 +5,18 @@ var prom = new Promise((resolve, reject)=>{
 prom.then((result)=>{
     console.log(result)
 })
+
+var array1 = [12,33,2,4]
+for (var i of array1){
+    console.log(i)
+}
+
+var obj1 = {
+  name: 'John',
+  age: 30,
+  city: 'New York'
+}
+
+for (var i in obj1){
+    console.log(obj1[i])
+}
