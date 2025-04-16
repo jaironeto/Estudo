2a323eba114ca50dd72026c2b5c834615e8e6e69
jairo neto <jaironeto788@gmail.com>
2025-04-15 21:03:46 -0300

    modified global and z.js

--- z.js
+++ z.js
@@ -1,22 +1,7 @@
-var prom = new Promise((resolve, reject)=>{
-    setTimeout(()=>{resolve('done')}, 1000)
-})
-
-prom.then((result)=>{
-    console.log(result)
-})
-
-var array1 = [12,33,2,4]
-for (var i of array1){
-    console.log(i)
+var teste = "z";
+var obj = {
+  a: 1,
+  [teste]: 23
 }
 
-var obj1 = {
-  name: 'John',
-  age: 30,
-  city: 'New York'
-}
-
-for (var i in obj1){
-    console.log(obj1[i])
-}
+console.log(obj.z);
