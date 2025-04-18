e6465567bb26ba77b165b13a47bbe02e2221ab08
jairo neto <jaironeto788@gmail.com>
2025-04-17 21:06:31 -0300

    modified z.html and z.js in .

--- z.js
+++ z.js
@@ -1,7 +1,15 @@
-var teste = "z";
-var obj = {
-  a: 1,
-  [teste]: 23
+function Teste(a){
+  this.a = a;
+  this.b = function() {
+    return this.a;
+  }
 }
 
-console.log(obj.z);
+var obj = new Teste(1);
+console.log(obj.b()); // 1
+
+Teste.prototype.func = function(){
+  return this.a + 3;
+}
+
+console.log(obj.func()); // 4
